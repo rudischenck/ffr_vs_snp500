4548a3c61a9c30709f9cf90866d82b3e2a953dff
rudi schenck <rudische@gmail.com>
2019-03-13 20:18:15 -0400

    Dataframe combine, some comments and plotting

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import sqlite3 as sql
 import seaborn as sns
@@ -15,15 +16,17 @@ def dataframedb(csvfile, tablename, dbname, querystring):
 
 
 #%%
+# Federal Funds Dataframe, SQL
 fftuple = ("FEDFUNDS.csv",
            "ff_table",
            "ff.db",
            'SELECT STRFTIME("%Y-%m", "DATE") AS FDate, FEDFUNDS FROM ff_table'
            )
 ff_df = dataframedb(*fftuple)
-#ff_df
+#ff_df.dtypes
 
 #%%
+# GDP Dataframe, SQL
 gdptuple = ("A191RL1Q225SBEA.csv",
             "gdp_table",
             "gdp.db",
@@ -33,17 +36,27 @@ gdp_df = dataframedb(*gdptuple)
 #gdp_df
 
 #%%
+# SNP 500 Dataframe, SQL
 snp_tuple = ("^GSPC.csv",
              "snp_table",
              "snp.db",
              'SELECT STRFTIME("%Y-%m", Date) AS FDate, MAX(Close) AS maxclose FROM snp_table GROUP BY FDate HAVING FDate >= "1954-07"'
              )
 snp_df = dataframedb(*snp_tuple)
+# SNP 500 % change month over month
 snp_pc_df = pd.DataFrame({
     'FDate': snp_df['FDate'],
     'Percent Change': snp_df['maxclose'].pct_change() * 100
 })
-snp_pc_df
+snp_pc_df.drop(snp_pc_df.index[0], inplace=True)
+#snp_pc_df.dtypes
+#%%
+# Combine Dataframes
+gdp_df.drop(gdp_df.index[0], inplace=True)
+ff_df.drop(ff_df.index[0], inplace=True)
+combined = pd.concat([snp_pc_df, gdp_df, ff_df], sort=False)
+combined['FDate'] = pd.to_datetime(combined['FDate'], infer_datetime_format=True)
+df_combined = combined.groupby("FDate").mean()
 
 #%%
 gdp_df['FDate'] = pd.to_datetime(gdp_df['FDate'], infer_datetime_format=True)
@@ -64,6 +77,5 @@ snp_pc_df['FDate'] = pd.to_datetime(snp_pc_df['FDate'], infer_datetime_format=Tr
 g = sns.relplot(x='FDate', y='Percent Change', kind='line', data=snp_pc_df)
 g.fig.autofmt_xdate()
 #%%
-df = pd.merge(snp_df, ff_df, on="FDate")
-g = sns.relplot(x='FDate', y="FEDFUNDS", hue="maxclose", kind="line", data=df)
-g.fig.autofmt_xdate()
+sns.regplot(x='GDP', y='FEDFUNDS', data=df_combined)
+#sns.regplot(x='GDP', y='Percent Change', data=df_combined)
